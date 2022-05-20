e9ff8227637d60ffb8519a047a61a64641bb0fb3
Junior Sott <sottjr@gmail.com>
2022-05-20 05:36:00 +0200

    add: criada rota salao

---
+++ ws/src/models/relationship/salaoColaborador.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const salaoColaborador = new Schema({
+    salaoId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Salao',
+        required: true
+    },
+
+    colaboradorId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Colaborador',
+        required: true
+    },
+    status: {
+        type: String,
+        required: [true, "Ativo/Inativo obrigatório"],
+        enum: ["A", "I", "E"],
+        default: "A"
+    },
+    dataCadastro: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+
+module.exports = mongoose.model('SalaoColaborador', salaoColaborador)
