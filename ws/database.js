f63da035d2f5be9fff1a845dacbd6bae18bf1b3e
Junior Sott <sottjr@gmail.com>
2022-05-23 01:53:27 +0200

    add: code clean

--- ws/database.js
+++ ws/database.js
@@ -3,11 +3,4 @@ const mongoose = require('mongoose');
 const URI = process.env.URI
 
 
-// MONGO DB ATT Não utiliza-se mais.
-//https://mongoosejs.com/docs/5.x/docs/deprecations.html
-// mongoose.set('useNewUrlParser', true);
-// mongoose.set('useFindAndModify', false);
-// mongoose.set('useCreateIndex', true);
-// mongoose.set('useUnifiedTopology', true);
-
 mongoose.connect(URI).then(() => console.log("DB is UP")).catch(() => console.log(err));
