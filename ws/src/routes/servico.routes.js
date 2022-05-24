94f3c79e158005881492938ff79e98226c9a1fc8
Junior Sott <sottjr@gmail.com>
2022-05-24 20:24:20 +0200

    add: adicionado método put na rota serviços.

--- ws/src/routes/servico.routes.js
+++ ws/src/routes/servico.routes.js
@@ -48,7 +48,7 @@ router.post("/", async (req, res) => {
 
             //criar service
             let jsonService = JSON.parse(servico);
-           const servicoCadastrado = await Servico(jsonService).save();
+            const servicoCadastrado = await Servico(jsonService).save();
 
             //Criar arquivo
             arquivos = arquivos.map(arquivo => ({
@@ -70,5 +70,73 @@ router.post("/", async (req, res) => {
     req.pipe(busboy);
 
 });
-''
+
+router.put("/:id", async (req, res) => {
+    let busboy = new Busboy(
+        { headers: req.headers }
+    );
+    busboy.on("finish", async () => {
+        try {
+            const { salaoId, servico } = req.body;
+            let errors = [];
+            let arquivos = [];
+            // console.log(req.files)
+            if (req.files && Object.keys(req.files).length > 0) {
+                for (let key of Object.keys(req.files)) {
+                    const file = req.files[key];
+
+                    const nameParts = file.name.split(".");
+                    const fileName = `${new Date().getTime()}.${nameParts[nameParts.length - 1]
+                        }`;
+                    const path = `servicos/${salaoId}/${fileName}`;
+
+                    const response = await aws.uploadToS3(file, path);
+
+                    if (response.error) {
+                        errors.push({
+                            error: true,
+                            message: response.message
+                        })
+                    } else {
+                        arquivos.push(path)
+                    }
+
+
+                }
+            };
+            if (errors.length > 0) {
+                res.json(errors[0]);
+                return false;
+            };
+
+            //criar service
+            const jsonServico = JSON.parse(servico)
+            await Servico.findByIdAndUpdate(
+                req.params.id,
+                jsonServico
+            );
+
+            //Criar arquivo
+            arquivos = arquivos.map(arquivo => ({
+                referenciaId: req.params.id,
+                model: "Servico",
+                caminho: arquivo
+            }));
+
+            await Arquivo.insertMany(arquivos);
+
+            res.json({
+                error: false
+            })
+
+        } catch (err) {
+            res.json({ error: true, message: err.message });
+        };
+    });
+    req.pipe(busboy);
+
+});
+
+
+
 module.exports = router;
